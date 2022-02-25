7515809dbb580d5b69d119f3c4ba315e95935dad
kimiaFkh <kimia.fakhari@ut.ac.ir>
2022-02-25 22:10:50 +0330

    Movie_Cast

--- src/main/java/Entity/Movie.java
+++ src/main/java/Entity/Movie.java
@@ -4,15 +4,21 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Movie {
-    public int getId() {
-        return id;
-    }
+    private int id;
+    private String name;
+    private String summary;
+    private String releaseDate;
+    private String director;
+    private int totalLikes;
+    private int totalDislikes;
+    private List<Integer> cast = new ArrayList<>();
+    private float imdbRate;
+    private String duration;
+    private int ageLimit;
+    private List<String> writers = new ArrayList<>();
+    private List<String> genres = new ArrayList<>();
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public Movie(int id, String name, String summary, String releaseDate, String director, List<String> writers, List<String> genres, List<Actor> cast, float imdbRate, int duration, int ageLimit) {
+    public Movie(int id, String name, String summary, String releaseDate, String director, List<String> writers, List<String> genres, List<Integer> cast, float imdbRate, String duration, int ageLimit) {
         this.id = id;
         this.name = name;
         this.summary = summary;
@@ -26,23 +32,21 @@ public class Movie {
         this.ageLimit = ageLimit;
     }
 
-    private int id;
-    private String name;
-    private String summary;
-    private String releaseDate;
-    private String director;
-    private List<String> writers = new ArrayList<>();
-    private List<String> genres = new ArrayList<>();
-    public List<Actor> getCast() {
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public List<Integer> getCast() {
         return cast;
     }
-    public void setCast(List<Actor> cast) {
+    public void setCast(List<Integer> cast) {
         this.cast = cast;
     }
-    private List<Actor> cast = new ArrayList<>();
-    private float imdbRate;
-    private int duration;
-    private int ageLimit;
+
     public boolean isSame(Movie movie) {
         return this.id == movie.id;
     }
