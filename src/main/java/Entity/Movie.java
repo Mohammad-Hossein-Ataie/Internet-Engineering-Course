77bc9cebcccbb82d38ca1d1a29a4548f320cceb6
Mohammad-Hossein-Ataie <mh.ataie@ut.ac.ir>
2022-02-25 21:25:35 +0330

    Add constructor for movie

--- src/main/java/Entity/Movie.java
+++ src/main/java/Entity/Movie.java
@@ -12,6 +12,20 @@ public class Movie {
         this.id = id;
     }
 
+    public Movie(int id, String name, String summary, String releaseDate, String director, List<String> writers, List<String> genres, List<Actor> cast, float imdbRate, int duration, int ageLimit) {
+        this.id = id;
+        this.name = name;
+        this.summary = summary;
+        this.releaseDate = releaseDate;
+        this.director = director;
+        this.writers = writers;
+        this.genres = genres;
+        this.cast = cast;
+        this.imdbRate = imdbRate;
+        this.duration = duration;
+        this.ageLimit = ageLimit;
+    }
+
     private int id;
     private String name;
     private String summary;
@@ -19,15 +33,12 @@ public class Movie {
     private String director;
     private List<String> writers = new ArrayList<>();
     private List<String> genres = new ArrayList<>();
-
     public List<Actor> getCast() {
         return cast;
     }
-
     public void setCast(List<Actor> cast) {
         this.cast = cast;
     }
-
     private List<Actor> cast = new ArrayList<>();
     private float imdbRate;
     private int duration;
