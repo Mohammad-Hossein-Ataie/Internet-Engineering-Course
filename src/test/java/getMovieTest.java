daffefa9b3a2609570cbf2dfa1399661f38b41a8
kimiaFkh <kimia.fakhari@ut.ac.ir>
2022-02-25 23:52:40 +0330

    test

--- src/test/java/getMovieTest.java
+++ src/test/java/getMovieTest.java
@@ -1,10 +1,26 @@
 import Controller.MovieController;
 import Entity.Movie;
 import org.junit.Before;
+import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 
 public class getMovieTest {
-    //private static Movie movie = new Movie(1, "Film", "Action Film!", "2020-10-10", "John Star", new ArrayList<>(Arrays.asList("John Son")), ["Action"], 1, "6", "100");
+    private static Movie movie;
+    @Before
+    public static void deforeTest() {
+        try {
+            movie = new Movie(1, "Film", "Action Film!", "2020-10-10", "John Star", new ArrayList<>(Arrays.asList("John Son")), new ArrayList<>(Arrays.asList("Action")), 1, 6.2, "100");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+
+    @Test
+    public void Test() {
+        //
+    }
+
 }
