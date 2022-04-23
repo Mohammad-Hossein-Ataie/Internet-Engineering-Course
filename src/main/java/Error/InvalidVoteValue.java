279b9802a4cb3e35a79be52cf25269ac1d9b6ed2
kimiaFkh <kimia.fakhari@ut.ac.ir>
2022-04-23 11:33:34 +0430

    ui init

--- src/main/java/Error/InvalidVoteValue.java
+++
@@ -1,9 +0,0 @@
-package Error;
-
-public class InvalidVoteValue extends ClientError{
-    public String getMessage() {
-        return message;
-    }
-
-    private static final String message = "InvalidVoteValue";
-}
