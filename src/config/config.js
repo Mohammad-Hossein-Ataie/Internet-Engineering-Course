302a3fc07a8b6296600b0f5a8d26ed630ce2e028
kimiaFkh <kimia.fakhari@ut.ac.ir>
2022-04-27 10:47:38 +0430

    adding config

--- src/config/config.js
+++ src/config/config.js
@@ -2,6 +2,6 @@ export const DOMAIN = "http://localhost:8080/";
 export let USER_LOGIN_URL = DOMAIN + "login/";
 export let USER_LOGOUT_URL = DOMAIN + "logout/";
 export let MOVIES_URL = DOMAIN + "movies/";
-// export let MOVIE_URL = DOMAIN + "movies/";
+export let MOVIE_URL = DOMAIN + "movies/";
 export let ACTORS_URL = DOMAIN + "actors/";
 export let WATCHLIST_URL = DOMAIN + "watchlist";
