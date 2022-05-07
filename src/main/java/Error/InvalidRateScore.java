039938bece2915e91cda35044577d64c817bbab3
Mohammad-Hossein-Ataie <mh.ataie@ut.ac.ir>
2022-05-07 10:47:18 +0430

    changed

--- src/main/java/Error/InvalidRateScore.java
+++
@@ -1,9 +0,0 @@
-package Error;
-
-public class InvalidRateScore extends ClientError {
-    public String getMessage() {
-        return message;
-    }
-
-    private static final String message = "InvalidRateScore";
-}
--- src/main/java/Error/MovieNotExist.java
+++
@@ -1,9 +0,0 @@
-package Error;
-
-public class MovieNotExist extends ClientError{
-    public String getMessage() {
-        return message;
-    }
-
-    private static final String message = "Movie not exist!";
-}
